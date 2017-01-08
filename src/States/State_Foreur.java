bf3b677d105dec26c607620c8dab182436957984
loupkreidl <loupkreidl@mbp-de-loup>
2017-01-08 01:23:02 +0100

    implé du state foreur

--- src/States/State_Foreur.java
+++ src/States/State_Foreur.java
@@ -15,14 +15,17 @@ public class State_Foreur extends State{
 	@Override
 	public void step(Lemming lem) {
 		if(this.duree>0){
-			this.duree--;
-			lem.setPosY(lem.getPosY()+1);
-			String bloc=jeu.getObstacle(lem.getPosX(), lem.getPosY());
+			String bloc=jeu.getObstacle(lem.getPosX(), lem.getPosY()+1);
 			if(bloc.compareTo("OTerre")==0){
 				jeu.destroyObstacle(lem.getPosX(), lem.getPosY());
+				this.duree--;
+				lem.setPosY(lem.getPosY()+1);
 			}else if(bloc.compareTo("OMetal")==0){
 				this.duree=0;
 			}
+		}
+		if(this.duree<=0){
+			lem.setState(new State_Marcheur(jeu));
 		}
 	}
 }
